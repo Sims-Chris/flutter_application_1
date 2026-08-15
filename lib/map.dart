27facad64bead37c7f41c189180295348f8567a4
Sims-Chris <chrissims@live.co.uk>
2026-08-15 22:22:11 +0100

    Added Data to spots

--- lib/map.dart
+++ lib/map.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/widgets/sidebar.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -9,7 +11,21 @@ class MapSpot {
 	final int id;
 	final LatLng point;
 
-	MapSpot({required this.id, required this.point});
+	final String name;
+	final String description;
+	final int rating;
+
+	final List<String> tags;
+
+
+	MapSpot({
+		required this.id, 
+		required this.point, 
+		required this.name,
+		required this.description, 
+		required this.rating, 
+		required this.tags
+	});
 }
 
 class MapScreen extends StatefulWidget{
@@ -55,31 +71,137 @@ class _MapScreenState extends State<MapScreen> {
 			_openExistingSpot(tappedSpot);
 		} 
 		else {
-			_addNewSpot(point);
+			_confirmNewSpot(point);
 		}
   	}
+
+	Future<void> _confirmNewSpot(LatLng point) async {
+		final bool? shouldCreate = await showDialog<bool>(
+			context: context,
+
+			builder: (BuildContext context){
+				return AlertDialog(
+					title: const Text("New Spot"),
+					content: const Text("Open a new Spot Here?"),
+					actions: [
+						TextButton(
+							onPressed: () => Navigator.pop(context, false),
+							child: const Text("No"),
+						),
+						TextButton(
+							onPressed: () => Navigator.pop(context, true),
+							child: const Text("Yes")
+						),
+					],
+				);
+			},
+		);
+
+		if (shouldCreate == true){
+			_enterSpotDetails(point);
+		}
+	}
+
+	Future<void> _enterSpotDetails(LatLng point) async{
+		final nameController = TextEditingController();
+		final descriptionController = TextEditingController();
+		final ratingController = TextEditingController();
+		final tagsController = TextEditingController();
+
+		final bool? shouldSave = await showDialog<bool>(
+			context: context, 
+			builder: (BuildContext context) {
+				return AlertDialog(
+					title: const Text("New Spot Details"),
+					content: SingleChildScrollView(
+						child: Column(
+							mainAxisSize: MainAxisSize.min,
+
+							children: [
+								TextField(
+									controller: nameController,
+									decoration: const InputDecoration(labelText: "Name"),
+								),
+								TextField(
+									controller: descriptionController,
+									decoration: const InputDecoration(labelText: "Description"),
+								),
+								TextField(
+									controller: ratingController,
+									decoration: const InputDecoration(labelText: "Rating 1-5"),
+								),
+								TextField(
+									controller: tagsController,
+									decoration: const InputDecoration(labelText: "Tags (Comma seperated)"),
+								),
+							],
+						),
+					),
+
+					actions: [
+						TextButton(
+							onPressed: () => Navigator.pop(context, false), 
+							child: const Text("Cancel"),
+							),
+						TextButton(
+							onPressed: () => Navigator.pop(context, true),
+						 	child: const Text("Save"),
+						 ),
+					],
+				);
+			},
+		);
+
+		if (shouldSave == true) {
+			setState(() {
+				final newId = DateTime.now().millisecondsSinceEpoch;
+
+				final int parsedRating = int.tryParse(ratingController.text) ?? 0; // Defaults to 0 if fails to parse
+
+				final List<String> parsedTags = tagsController.text
+					.split(',')
+					.map((tag) => tag.trim())
+					.where((tag) => tag.isNotEmpty)
+					.toList();
+
+				_spots.add(
+					MapSpot(id: newId,
+					 	point: point,
+						name: nameController.text,
+						description: descriptionController.text,
+						rating: parsedRating,
+					   	tags: parsedTags
+					),
+				);
+			});
+		}
+	}
+
 
 	void _openExistingSpot(MapSpot spot){
 		debugPrint("Existing spot tapped, with ID: ${spot.id}");
 
 		ScaffoldMessenger.of(context).showSnackBar(
 			SnackBar(
-				content: Text("Opened spot: ${spot.id}"),
+				content: Text("Opened spot: ${spot.id} \n ${spot.description}"),
+
 				duration: const Duration(seconds: 5),
 			),
 		);
 	}
 
-	void _addNewSpot (LatLng point){
-		setState(() {
-
-			final newId = DateTime.now().millisecondsSinceEpoch;
-
-			_spots.add(
-				MapSpot(id: newId, point: point)
-			);
-		});
-	}
+	// Legacy add New Spot 
+	//void _addNewSpot (LatLng point){
+	// 	setState(() {
+	// 		final newId = DateTime.now().millisecondsSinceEpoch;
+	// 		final String description = "Test Description";
+	// 		final int rating = 5;
+	// 		final List<String> tags = ["tag1", "tag2"];
+	// 		_spots.add(
+	// 			MapSpot(id: newId, point: point, description: description, rating: rating, tags: tags)
+	// 		);
+	// 	});
+	// }
 
 	@override
 	Widget build(BuildContext context){
